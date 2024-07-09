2ed8eff346bb2e6ef4ddad5ddcff76c8b4419eb5
TrDungxx <Hovercard27@gmail.com>
2024-07-10 01:52:51 +0700

    line1

--- src/App.js
+++ src/App.js
@@ -1,34 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-import Welcome from './component/Welcomevd2'
-import Uservd2 from './component/Uservd2';
-function App (){
-  const users = [
-    {
-      name: "Cuban",
-      pic:"https://images.unsplash.com/photo-1618641986557-1ecd230959aa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80",
-    },
-    {
-      name: "Robert",
-      pic: "https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80",
-      },
-      {
-        name: "Einstein",
-        pic: "https://media.istockphoto.com/id/1179420343/photo/smiling-man-outdoors-in-the-city.jpg?s=612x612&w=0&k=20&c=8l-gOboGEFSyCFXr09EguDmV0E0bFT5usAms1wyFBh8=",
-      },
-  ];
-  return (
-    <div className="App">
-    {users.map((usr) => (
-    <Uservd2 name={usr.name} pic={usr.pic} />
-    ))}
-    </div>
-    );
-   
+import React from "react";
+
+import Run1 from "./component/run1";
+
+
+const App = () => {
+return (
+<div>
+  <div className='Staffservicepage'>
+    
+<Run1 />
+
+ </div>
+ </div>
+);
 }
- 
-  export default App;
- 
+
+export default App;
  
  
 
---
+++ src/component/run1.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./run1.css";
+import "./background.css";
+import Line1 from "./line1";
+
+
+function Run1() {
+  return (
+    <section>
+      <div className="Staffservicepage">
+        <Line1 />
+        {/* <Event2 />
+        <Event3 />
+        <Event4 />
+        <Event6 />
+        <Event7 />
+        <Form /> */}
+      </div>
+    </section>
+  );
+}
+export default Run1;
