d66d7a457ba990c557b353c17cd0c0d6d31219ae
TrDungxx <Hovercard27@gmail.com>
2024-07-11 21:50:09 +0700

    123

--- src/componentTrDung/run1.js
+++ src/componentTrDung/run1.js
@@ -1,7 +1,15 @@
 import React from "react";
 import "./run1.css";
 import "./background.css";
+import "./form.css";
 import Line1 from "./line1";
+import Line2 from "./line2";
+import Line3 from "./line3";
+import Line4 from "./line4";
+import Line5 from "./line5";
+import Form from "./line6";
+
+
 
 
 function Run1() {
@@ -9,12 +17,11 @@ function Run1() {
     <section>
       <div className="Staffservicepage">
         <Line1 />
-        {/* <Event2 />
-        <Event3 />
-        <Event4 />
-        <Event6 />
-        <Event7 />
-        <Form /> */}
+        <Line2/>
+        <Line3/>
+        <Line4/>
+        <Line5/>
+       <Form/>
       </div>
     </section>
   );
